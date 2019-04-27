415a4b0b00d52c3e0e7d070c769e4d176d6d85a5
Michael James <mikejlyh@gmail.com>
2019-04-27 13:47:52 +0100

    setting homepage

---
+++ client/build/precache-manifest.01f2ad05dd31ff51822215a7afadb03e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8b05985e86bdc3698404",
+    "url": "%20//mivechatserver.herokuapp.com/static/js/runtime~main.2a27adb7.js"
+  },
+  {
+    "revision": "9dda64fad966d244a8c1",
+    "url": "%20//mivechatserver.herokuapp.com/static/js/main.4a399b19.chunk.js"
+  },
+  {
+    "revision": "380e7507e8eb4a745d08",
+    "url": "%20//mivechatserver.herokuapp.com/static/js/2.d1e4ddde.chunk.js"
+  },
+  {
+    "revision": "9dda64fad966d244a8c1",
+    "url": "%20//mivechatserver.herokuapp.com/static/css/main.627c13a9.chunk.css"
+  },
+  {
+    "revision": "9d796495b2d34155cfcf66f51894f0ec",
+    "url": "%20//mivechatserver.herokuapp.com/index.html"
+  }
+];
